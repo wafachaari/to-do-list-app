65db35497025f7f9abc9f6f7760846217eed7411
wafachaari <wafachaari30@gmail.com>
2021-03-10 15:09:30 +0100

    Create scripts.js
    if applied, this commit will have converted a javascripts code into jquery

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+function newItem(){
+
+//javascript
+//1. Adding a new item to the list of items:
+  let li = $("<li></li>");
+let inputValue=$("#input").val();
+li.append(inputValue);
+   if (inputValue === '') {
+     alert("You must write something!");
+   } else {
+     $("#list").append(li);
+   }
+
+ //2. Crossing out an item from the list of items:
+
+ 	li.dblclick (() => li.toggleclass("strike"));
+
+ //3(i). Adding the delete button "X":
+   let crossOutButton =$("<crossOutButton></crossOutButton>");
+ 	crossOutButton.append(document.createTextNode("X"));
+ 	li.append(crossOutButton);
+ 	crossOutButton.click(() => deleteListItem()  );
+ //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+   function deleteListItem(){
+ 		li.addClass("delete")
+ 	}
+ // 4. Reordering the items:
+   $('#list').sortable();
+
+}
